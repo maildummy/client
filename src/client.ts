6dd923c82a852d6e2e7e130548789a1ff3293801
Michel Schreurs <michel@schreurs.consulting>
2022-10-26 06:59:16 +0200

    chore: add prettierrc and format code

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
-import fetch from "cross-fetch";
-import { Inbox, MailMetadata, Mail, List, ClientConfig } from "./types";
+import fetch from 'cross-fetch';
+import { Inbox, MailMetadata, Mail, List, ClientConfig } from './types';
 
-const domain = process?.env?.MAILDUMMY_DOMAIN || "maildummy.io";
+const domain = process?.env?.MAILDUMMY_DOMAIN || 'maildummy.io';
 
 export class MaildummyClient {
   private mails: { [key: string]: string[] } = {};
@@ -9,9 +9,7 @@ export class MaildummyClient {
   constructor(config?: ClientConfig) {
     const apiKey = config?.apiKey || process.env.MAILDUMMY_API_KEY;
     if (!apiKey) {
-      throw Error(
-        "No API key provided, please provide the apiKey config option, or set the MAILDUMMY_API_KEY environment variable"
-      );
+      throw Error('No API key provided, please provide the apiKey config option, or set the MAILDUMMY_API_KEY environment variable');
     }
     this.apiKey = apiKey;
   }
@@ -22,7 +20,7 @@ export class MaildummyClient {
    */
   createInbox = async (): Promise<Inbox> => {
     const res = await fetch(`https://api.${domain}/v1/inboxes`, {
-      method: "POST",
+      method: 'POST',
       headers: { Authorization: this.apiKey },
     });
 
@@ -43,7 +41,7 @@ export class MaildummyClient {
    */
   deleteInbox = async (uuid: string): Promise<boolean> => {
     const res = await fetch(`https://api.${domain}/v1/inboxes/${uuid}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: { Authorization: this.apiKey },
     });
 
@@ -55,12 +53,10 @@ export class MaildummyClient {
    * @param pageToken Provide a nextPageToken from a previous response here, to fetch the next page
    * @returns Promise<Inbox>
    */
-  listInboxes = async (
-    pageToken?: string
-  ): Promise<List<{ inboxes: Inbox[] }>> => {
+  listInboxes = async (pageToken?: string): Promise<List<{ inboxes: Inbox[] }>> => {
     const url = new URL(`https://api.${domain}/v1/inboxes`);
     if (pageToken) {
-      url.searchParams.append("next_page", pageToken);
+      url.searchParams.append('next_page', pageToken);
     }
     const res = await fetch(url, {
       headers: { Authorization: this.apiKey },
@@ -105,7 +101,7 @@ export class MaildummyClient {
    */
   deleteMail = async (uuid: string): Promise<boolean> => {
     const res = await fetch(`https://api.${domain}/v1/mails/${uuid}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: { Authorization: this.apiKey },
     });
 
@@ -118,13 +114,10 @@ export class MaildummyClient {
    * @param pageToken Provide a nextPageToken from a previous response here, to fetch the next page
    * @returns Promise<Inbox>
    */
-  listMails = async (
-    inboxUuid: string,
-    pageToken?: string
-  ): Promise<List<{ mails: MailMetadata[] }>> => {
+  listMails = async (inboxUuid: string, pageToken?: string): Promise<List<{ mails: MailMetadata[] }>> => {
     const url = new URL(`https://api.${domain}/v1/mails/`);
     if (pageToken) {
-      url.searchParams.append("next_page", pageToken);
+      url.searchParams.append('next_page', pageToken);
     }
     const res = await fetch(url, {
       headers: { Authorization: this.apiKey },
@@ -151,10 +144,7 @@ export class MaildummyClient {
    * @param inboxUuid inbox to fetch emails for
    * @param timeoutInSeconds time to wait for a new email to arrive, when none are received, an error is thrown
    */
-  waitForNewMails = async (
-    inboxUuid: string,
-    timeoutInSeconds = 10
-  ): Promise<MailMetadata[]> => {
+  waitForNewMails = async (inboxUuid: string, timeoutInSeconds = 10): Promise<MailMetadata[]> => {
     return new Promise((resolve, reject) => {
       let currentTimeout: any;
       const timeout = setTimeout(() => {
@@ -164,12 +154,9 @@ export class MaildummyClient {
       }, timeoutInSeconds * 1000);
 
       const getNewMails = async () => {
-        const res = await fetch(
-          `https://api.${domain}/v1/mails/inbox/${inboxUuid}`,
-          {
-            headers: { Authorization: this.apiKey },
-          }
-        );
+        const res = await fetch(`https://api.${domain}/v1/mails/inbox/${inboxUuid}`, {
+          headers: { Authorization: this.apiKey },
+        });
 
         const { data } = await res.json();
         const mails: MailMetadata[] = data.mails.map((raw: any) => {
@@ -180,9 +167,7 @@ export class MaildummyClient {
           } as MailMetadata;
         });
 
-        const newMails = mails?.filter(
-          (mail: MailMetadata) => !this.mails[inboxUuid]?.includes(mail.uuid)
-        );
+        const newMails = mails?.filter((mail: MailMetadata) => !this.mails[inboxUuid]?.includes(mail.uuid));
         if (newMails.length) {
           clearTimeout(timeout);
 
@@ -190,10 +175,7 @@ export class MaildummyClient {
           if (!this.mails[inboxUuid]) {
             this.mails[inboxUuid] = [];
           }
-          this.mails[inboxUuid] = [
-            ...this.mails[inboxUuid],
-            ...newMails.map((mail) => mail.uuid),
-          ];
+          this.mails[inboxUuid] = [...this.mails[inboxUuid], ...newMails.map((mail) => mail.uuid)];
 
           resolve(newMails);
 
